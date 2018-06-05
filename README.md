e7f4ae709c9406b2633570ad22d4bc1e277d1c1e
Tom Greenfield <tom.greenfield@kineo.com>
2018-06-05 16:37:09 +0100

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# adapt-drop
-A dropdown extension to expand and shrink elements
+# Drop
+
+A dropdown extension to expand and shrink elements.
+
+## Installation
+
+* Add the [example JSON](example.json) to elements you wish to shrink.
+* Copy the extension folder into the src > extensions directory and run an appropriate Grunt task.
+
+## Usage
+
+* Select the buttons to expand and shrink elements.
+
+## Attributes
+
+<table>
+	<tr>
+		<th>Attribute</th>
+		<th>Type</th>
+		<th>Description</th>
+		<th>Default</th>
+	</tr>
+	<tr>
+		<td><code>_isEnabled</code></td>
+		<td>String</td>
+		<td>Adds a dropdown button to expand and shrink the element</td>
+		<td><code>false</code></td>
+	</tr>
+	<tr>
+		<td><code>_classes</code></td>
+		<td>String</td>
+		<td>Custom classes to add to the container</td>
+		<td><code>""</code></td>
+	</tr>
+	<tr>
+		<td><code>title</code></td>
+		<td>String</td>
+		<td>Button text</td>
+		<td><code>""</code></td>
+	</tr>
+	<tr>
+		<td><code>ariaLabel</code></td>
+		<td>String</td>
+		<td>Button label for assistive technologies</td>
+		<td><code>""</code></td>
+	</tr>
+</table>
---
+++ js/adapt-drop.js
@@ -0,0 +1,92 @@
+define([ "core/js/adapt" ], function(Adapt) {
+
+	var DropView = Backbone.View.extend({
+
+		className: function() {
+			var classes = "drop";
+			var modelClasses = this.model.get("_drop")._classes;
+
+			if (modelClasses) classes += " " + modelClasses;
+
+			return classes;
+		},
+
+		$content: null,
+
+		initialize: function(options) {
+			this.$content = options.$content;
+			this.listenTo(Adapt, "remove", this.remove).render();
+		},
+
+		events: {
+			"click .drop-button": "onClick"
+		},
+
+		render: function() {
+			var template = Handlebars.templates.drop;
+			var data = this.model.get("_drop");
+
+			this.$el.html(template(data)).appendTo(this.$content.parent());
+			this.$content.addClass("drop-content").appendTo(this.$el).hide();
+			this.setChildrenVisibility(false);
+		},
+
+		onClick: function() {
+			this.$el.addClass("visited");
+			this.toggle();
+
+			return false;
+		},
+
+		setChildrenVisibility: function(isVisible) {
+			this.model.getChildren().each(function(child) {
+				child.setOnChildren("_isVisible", isVisible, { pluginName: "_drop" });
+			});
+		},
+
+		toggle: function() {
+			var shouldOpen = !this.$el.hasClass("dropped");
+
+			this.$el
+				.toggleClass("dropped", shouldOpen)
+				.children(".drop-button").attr("aria-expanded", shouldOpen);
+
+			this.$content.stop().slideToggle({
+				start: (shouldOpen ? this.onOpen : this.onClose).bind(this),
+				done: (shouldOpen ? this.onOpened : this.onClosed).bind(this)
+			});
+		},
+
+		onOpen: function() {
+			$(window).trigger("resize");
+			Adapt.trigger("device:resize");
+			this.setChildrenVisibility(true);
+		},
+
+		onOpened: function() {
+			this.$content.a11y_focus();
+		},
+
+		onClose: function() {},
+
+		onClosed: function() {
+			this.setChildrenVisibility(false);
+		}
+
+	});
+
+	Adapt.once("app:dataReady", function() {
+		var views = [ "menu", "page", "article", "block", "component" ];
+		var eventList = views.map(function(view) { return view + "View:postRender"; });
+
+		Adapt.on(eventList.join(" "), function(view) {
+			var model = view.model;
+			var config = model.get("_drop");
+
+			if (config && config._isEnabled) {
+				new DropView({ model: model, $content: view.$el.children().first() });
+			}
+		});
+	});
+
+});
