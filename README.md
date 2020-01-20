63a4a6d8001b812637f8d8e9315eb6dc2f981001
montresor34 <>
2020-01-20 14:18:50 +0000

    Updated spacing and indentation; commented out less

--- README.md
+++ README.md
@@ -14,34 +14,34 @@ A dropdown extension to expand and shrink elements.
 ## Attributes
 
 <table>
-	<tr>
-		<th>Attribute</th>
-		<th>Type</th>
-		<th>Description</th>
-		<th>Default</th>
-	</tr>
-	<tr>
-		<td><code>_isEnabled</code></td>
-		<td>Boolean</td>
-		<td>Add a dropdown button to expand and shrink the element</td>
-		<td><code>false</code></td>
-	</tr>
-	<tr>
-		<td><code>_classes</code></td>
-		<td>String</td>
-		<td>Custom classes to add to the container</td>
-		<td><code>""</code></td>
-	</tr>
-	<tr>
-		<td><code>title</code></td>
-		<td>String</td>
-		<td>Button text</td>
-		<td><code>""</code></td>
-	</tr>
-	<tr>
-		<td><code>ariaLabel</code></td>
-		<td>String</td>
-		<td>Button label for assistive technologies</td>
-		<td><code>""</code></td>
-	</tr>
+  <tr>
+    <th>Attribute</th>
+    <th>Type</th>
+    <th>Description</th>
+    <th>Default</th>
+  </tr>
+  <tr>
+    <td><code>_isEnabled</code></td>
+    <td>Boolean</td>
+    <td>Add a dropdown button to expand and shrink the element</td>
+    <td><code>false</code></td>
+  </tr>
+  <tr>
+    <td><code>_classes</code></td>
+    <td>String</td>
+    <td>Custom classes to add to the container</td>
+    <td><code>""</code></td>
+  </tr>
+  <tr>
+    <td><code>title</code></td>
+    <td>String</td>
+    <td>Button text</td>
+    <td><code>""</code></td>
+  </tr>
+  <tr>
+    <td><code>ariaLabel</code></td>
+    <td>String</td>
+    <td>Button label for assistive technologies</td>
+    <td><code>""</code></td>
+  </tr>
 </table>
--- js/adapt-drop.js
+++ js/adapt-drop.js
@@ -1,91 +1,91 @@
 define([ "core/js/adapt" ], function(Adapt) {
 
-	var DropView = Backbone.View.extend({
+  var DropView = Backbone.View.extend({
 
-		$content: null,
+    $content: null,
 
-		className: function() {
-			var classes = "drop";
-			var modelClasses = this.model.get("_drop")._classes;
+    className: function() {
+      var classes = "drop";
+      var modelClasses = this.model.get("_drop")._classes;
 
-			if (modelClasses) classes += " " + modelClasses;
+      if (modelClasses) classes += " " + modelClasses;
 
-			return classes;
-		},
+      return classes;
+    },
 
-		initialize: function(options) {
-			this.$content = options.$content;
-			this.listenTo(Adapt, "remove", this.remove).render();
-		},
+    initialize: function(options) {
+      this.$content = options.$content;
+      this.listenTo(Adapt, "remove", this.remove).render();
+    },
 
-		events: {
-			"click .drop-button": "onClick"
-		},
+    events: {
+      "click .drop-button": "onClick"
+    },
 
-		render: function() {
-			var template = Handlebars.templates.drop;
-			var data = this.model.get("_drop");
+    render: function() {
+      var template = Handlebars.templates.drop;
+      var data = this.model.get("_drop");
 
-			this.$el.html(template(data)).appendTo(this.$content.parent());
-			this.$content.addClass("drop-content").appendTo(this.$el).hide();
-			this.setChildrenVisibility(false);
-		},
+      this.$el.html(template(data)).appendTo(this.$content.parent());
+      this.$content.addClass("drop-content").appendTo(this.$el).hide();
+      this.setChildrenVisibility(false);
+    },
 
-		onClick: function() {
-			this.$el.addClass("visited");
-			this.toggle();
+    onClick: function() {
+      this.$el.addClass("visited");
+      this.toggle();
 
-			return false;
-		},
+      return false;
+    },
 
-		setChildrenVisibility: function(isVisible) {
-			this.model.getChildren().each(function(child) {
-				child.setOnChildren("_isVisible", isVisible, { pluginName: "_drop" });
-			});
-		},
+    setChildrenVisibility: function(isVisible) {
+      this.model.getChildren().each(function(child) {
+        child.setOnChildren("_isVisible", isVisible, { pluginName: "_drop" });
+      });
+    },
 
-		toggle: function() {
-			var shouldOpen = !this.$el.hasClass("dropped");
+    toggle: function() {
+      var shouldOpen = !this.$el.hasClass("dropped");
 
-			this.$el.toggleClass("dropped", shouldOpen);
-			this.$(".drop-button").first().attr("aria-expanded", shouldOpen);
+      this.$el.toggleClass("dropped", shouldOpen);
+      this.$(".drop-button").first().attr("aria-expanded", shouldOpen);
 
-			this.$content.stop().slideToggle({
-				start: (shouldOpen ? this.onOpen : this.onClose).bind(this),
-				done: (shouldOpen ? this.onOpened : this.onClosed).bind(this)
-			});
-		},
+      this.$content.stop().slideToggle({
+        start: (shouldOpen ? this.onOpen : this.onClose).bind(this),
+        done: (shouldOpen ? this.onOpened : this.onClosed).bind(this)
+      });
+    },
 
-		onOpen: function() {
-			$(window).trigger("resize");
-			Adapt.trigger("device:resize");
-			this.setChildrenVisibility(true);
-		},
+    onOpen: function() {
+      $(window).trigger("resize");
+      Adapt.trigger("device:resize");
+      this.setChildrenVisibility(true);
+    },
 
-		onOpened: function() {
-			this.$content.a11y_focus();
-		},
+    onOpened: function() {
+      this.$content.a11y_focus();
+    },
 
-		onClose: function() {},
+    onClose: function() {},
 
-		onClosed: function() {
-			this.setChildrenVisibility(false);
-		}
+    onClosed: function() {
+      this.setChildrenVisibility(false);
+    }
 
-	});
+  });
 
-	Adapt.once("app:dataReady", function() {
-		var views = [ "menu", "page", "article", "block", "component" ];
-		var eventList = views.map(function(view) { return view + "View:postRender"; });
+  Adapt.once("app:dataReady", function() {
+    var views = [ "menu", "page", "article", "block", "component" ];
+    var eventList = views.map(function(view) { return view + "View:postRender"; });
 
-		Adapt.on(eventList.join(" "), function(view) {
-			var model = view.model;
-			var config = model.get("_drop");
+    Adapt.on(eventList.join(" "), function(view) {
+      var model = view.model;
+      var config = model.get("_drop");
 
-			if (config && config._isEnabled) {
-				new DropView({ model: model, $content: view.$el.children().first() });
-			}
-		});
-	});
+      if (config && config._isEnabled) {
+        new DropView({ model: model, $content: view.$el.children().first() });
+      }
+    });
+  });
 
 });
