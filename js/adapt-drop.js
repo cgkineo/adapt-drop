9aeceb8d4c6a0658f6096f7f4384cef713896240
Guy Willis <guy.willis@kineo.com>
2020-03-24 15:13:19 +0000

    Changed availability to visibility, added menuItem view, added theme less, tweaked hbs

--- js/adapt-drop.js
+++ js/adapt-drop.js
@@ -1,53 +1,60 @@
-define([ "core/js/adapt" ], function(Adapt) {
+define([
+  'core/js/adapt'
+], function(Adapt) {
 
   var DropView = Backbone.View.extend({
 
     $content: null,
 
     className: function() {
-      var classes = "drop is-closed";
-      var modelClasses = this.model.get("_drop")._classes;
+      var classes = 'drop is-closed';
+      var modelClasses = this.model.get('_drop')._classes;
 
-      if (modelClasses) classes += " " + modelClasses;
+      if (modelClasses) classes += ' ' + modelClasses;
 
       return classes;
     },
 
     initialize: function(options) {
       this.$content = options.$content;
-      this.listenTo(Adapt, "remove", this.remove).render();
+      this.listenTo(Adapt, 'remove', this.remove).render();
     },
 
     events: {
-      "click .js-toggle-view": "onClick"
+      'click .js-toggle-view': 'onClick'
     },
 
     render: function() {
       var template = Handlebars.templates.drop;
-      var data = this.model.get("_drop");
+      var data = this.model.get('_drop');
 
       this.$el.html(template(data)).appendTo(this.$content.parent());
-      this.$content.addClass("drop__content").appendTo(this.$el).hide();
-      this.setAvailability(false);
+      this.$content.addClass('drop__content').appendTo(this.$el).hide();
+      this.setChildrenVisibility(false);
     },
 
     onClick: function() {
-      this.$el.addClass("is-visited");
+      this.$el.addClass('is-visited');
       this.toggle();
 
       return false;
     },
 
-    setAvailability: function(isAvailable) {
-      this.model.set("_isAvailable", isAvailable);
+    setChildrenVisibility: function(isVisible) {
+      this.model.getChildren().each(function(child) {
+        child.setOnChildren("_isVisible", isVisible, { pluginName: "_drop" });
+      });
     },
 
     toggle: function() {
-      var shouldOpen = !this.$el.hasClass("is-open");
+      var shouldOpen = !this.$el.hasClass('is-open');
 
-      this.$el.toggleClass("is-open", shouldOpen)
-              .toggleClass("is-closed", !shouldOpen);
-      this.$(".drop__btn").first().attr("aria-expanded", shouldOpen);
+      this.$el
+        .toggleClass('is-open', shouldOpen)
+        .toggleClass('is-closed', !shouldOpen);
+      this.$('.drop__btn').first()
+        .addClass('is-visited')
+        .attr('aria-expanded', shouldOpen);
 
       this.$content.stop().slideToggle({
         start: (shouldOpen ? this.onOpen : this.onClose).bind(this),
@@ -56,9 +63,9 @@ define([ "core/js/adapt" ], function(Adapt) {
     },
 
     onOpen: function() {
-      $(window).trigger("resize");
-      Adapt.trigger("device:resize");
-      this.setAvailability(true);
+      $(window).trigger('resize');
+      Adapt.trigger('device:resize');
+      this.setChildrenVisibility(true);
     },
 
     onOpened: function() {
@@ -68,18 +75,18 @@ define([ "core/js/adapt" ], function(Adapt) {
     onClose: function() {},
 
     onClosed: function() {
-      this.setAvailability(false);
+      this.setChildrenVisibility(false);
     }
 
   });
 
-  Adapt.once("app:dataReady", function() {
-    var views = [ "menu", "page", "article", "block", "component" ];
-    var eventList = views.map(function(view) { return view + "View:postRender"; });
+  Adapt.once('app:dataReady', function() {
+    var views = [ 'menu', 'menuItem', 'page', 'article', 'block', 'component' ];
+    var eventList = views.map(function(view) { return view + 'View:postRender'; });
 
-    Adapt.on(eventList.join(" "), function(view) {
+    Adapt.on(eventList.join(' '), function(view) {
       var model = view.model;
-      var config = model.get("_drop");
+      var config = model.get('_drop');
 
       if (config && config._isEnabled) {
         new DropView({ model: model, $content: view.$el.children().first() });
